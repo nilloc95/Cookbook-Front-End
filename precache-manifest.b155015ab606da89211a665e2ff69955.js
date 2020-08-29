30f772e909c2ec9239920914fb4f1fac5a739116
Collin Gilmore <collingilmore@gmail.com>
2020-08-29 17:00:56 -0400

    Updates

---
+++ precache-manifest.b155015ab606da89211a665e2ff69955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468bb7b5fdd73daa625f793c6f438156",
+    "url": "/Cookbook-Front-End/index.html"
+  },
+  {
+    "revision": "2a69be2d2b2691374cce",
+    "url": "/Cookbook-Front-End/static/css/main.ce472751.chunk.css"
+  },
+  {
+    "revision": "25fbc17459b219a5d70e",
+    "url": "/Cookbook-Front-End/static/js/2.e6694ad4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cookbook-Front-End/static/js/2.e6694ad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a69be2d2b2691374cce",
+    "url": "/Cookbook-Front-End/static/js/main.2cc8e95a.chunk.js"
+  },
+  {
+    "revision": "a7c6e95d03d5c36e99f1",
+    "url": "/Cookbook-Front-End/static/js/runtime-main.656eebd8.js"
+  }
+]);
